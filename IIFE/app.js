8c94d048d3a70d368ef6a6eb3e226cb9916a82f4
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-02 20:06:08 -0500

    Add files via upload

---
+++ IIFE/app.js
@@ -0,0 +1,18 @@
+// immediately invoked function expressions
+
+(function () {
+    //logic here
+}) ();
+
+
+//normal function 
+function myFunction () {
+    //logic here
+}
+
+/*
+The primary reason to use an IIFE is to obtain data privacy.
+ Because JavaScript's var scopes variables to their containing
+  function, any variables declared within the IIFE cannot be
+   accessed by the outside world.
+ */
