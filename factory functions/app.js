f81f7a722d36b150784acd28c44c5da9be0b46d6
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-02 19:55:41 -0500

    Add files via upload

--- factory functions/app.js
+++ factory functions/app.js
@@ -80,3 +80,23 @@ const counterCreator = () => {
 
   Jeff.sayName();
   Jeff.doSomethingNerdy();
+
+  //the module pattern
+  const calculator = (() => {
+      const add = (a,b) => a+b;
+      const sub = (a,b) => a-b;
+      const mult = (a,b) => a*b;
+      const div = (a,b) => a/b;
+      return {
+          add,
+          sub,
+          mult,
+          div
+      };
+  })();
+console.log(
+   calculator.add(3,5),
+  calculator.sub(6,2),
+  calculator.mult(13,6)  
+)
+ 
