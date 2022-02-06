4360bf6821d0987719ec8f4085218e1d69d8aaca
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-06 18:07:30 -0500

    Add files via upload

---
+++ classes/script.js
@@ -0,0 +1,60 @@
+//the class synthax
+class myClass{
+   //classmethods
+constructor(){
+
+} 
+method1(){}
+method2(){}
+method3(){}
+}
+
+//example
+class User{
+    constructor(name){
+        this.name=name;
+    }
+
+    sayHi(){
+        console.log(this.name)
+    }
+}
+
+let user= new User('John')
+user.sayHi();
+
+console.log(typeof User) //function
+
+//rewriting class user in pure functions
+function User1(name){
+    this.name=name;
+}
+User1.prototype.greeting=function(){
+    console.log(`Hello! I am ${this.name}`);
+}
+
+let user1= new User1('Cory');
+user1.greeting();
+
+
+//class declarations
+class Rectangle{
+    constructor(height, width){
+        this.height=height;
+        this.width=width;
+    }
+
+    //getter
+    get area(){
+        return this.calcArea();
+    }
+
+    //method
+    calcArea(){
+        return this.height * this.width;
+    }
+};
+
+const square= new Rectangle(10,10)
+console.log(square.area);
+
