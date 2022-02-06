dbc3843d07088375a203e121fe529b57b5eb9a3c
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-06 18:12:03 -0500

    Add files via upload

--- classes/script.js
+++ classes/script.js
@@ -58,3 +58,19 @@ class Rectangle{
 const square= new Rectangle(10,10)
 console.log(square.area);
 
+//generator methods
+class Polygon{
+    constructor(...sides){
+        this.sides=sides;
+    }
+    //method
+    *getSides(){
+        for(const side of this.sides){
+            yield side;
+        }
+    }
+}
+
+const pentagon =new Polygon(1,2,3,4,5);
+console.log(([...pentagon.getSides()]));
+
